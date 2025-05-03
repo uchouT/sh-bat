650b87d315990774f70f6b1e807d57aad2777fd5
uchouT <xtx40@outlook.com>
2025-05-03 17:16:52 +0800

    refresh alist after uploaded

--- qb_auto.sh
+++ qb_auto.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 torrent_name=$1
 content_dir=$2
 root_dir=$3
 save_dir=$4
 files_num=$5
 torrent_size=$6
-file_hash=$7
+torrent_hash=$7
+torrent_type=$8
+
+# if [ ${torrent_type} == "ani-rss" ]
+# then
+# 	 exit 0
+# fi
 
 qb_version="4.5.4"
 qb_username="admin"
-qb_password="erzichibaba"
+qb_password="password"
 qb_web_url="http://localhost:8080"
-leeching_mode="false"
 log_dir="/root/qblog"
-rclone_dest="gensoufamily"
+rclone_dest="od"
 rclone_parallel="5"
-auto_del_flag="rcloned"
+uploaded_flag="rcloned"
+alist_host="http://localhost:5244"
+alist_token=""
+
 
 if [ ! -d ${log_dir} ]
 then
@@ -25,26 +33,26 @@ fi
 version=$(echo $qb_version | grep -P -o "([0-9]\.){2}[0-9]" | sed s/\\.//g)
 
 function qb_login(){
-	if [ ${version} -gt 404 ]
+	if [ "${version}" -gt 404 ]
 	then
 		qb_v="1"
 		cookie=$(curl -i --header "Referer: ${qb_web_url}" --data "username=${qb_username}&password=${qb_password}" "${qb_web_url}/api/v2/auth/login" | grep -P -o 'SID=\S{32}')
-		if [ -n ${cookie} ]
+		if [ -n "${cookie}" ]
 		then
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录成功！cookie:${cookie}" >> ${log_dir}/autodel.log
+			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录成功！cookie:${cookie}" >> ${log_dir}/qb_login.log
 
 		else
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录失败！" >> ${log_dir}/autodel.log
+			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录失败！" >> ${log_dir}/qb_login.log
 		fi
 	elif [[ ${version} -le 404 && ${version} -ge 320 ]]
 	then
 		qb_v="2"
 		cookie=$(curl -i --header "Referer: ${qb_web_url}" --data "username=${qb_username}&password=${qb_password}" "${qb_web_url}/login" | grep -P -o 'SID=\S{32}')
-		if [ -n ${cookie} ]
+		if [ -n "${cookie}" ]
 		then
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录成功！cookie:${cookie}" >> ${log_dir}/autodel.log
+			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录成功！cookie:${cookie}" >> ${log_dir}/qb_login.log
 		else
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录失败" >> ${log_dir}/autodel.log
+			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 登录失败" >> ${log_dir}/qb_login.log
 		fi
 	elif [[ ${version} -ge 310 && ${version} -lt 320 ]]
 	then
@@ -57,44 +65,59 @@ function qb_login(){
 	fi
 }
 
+function alist_refresh() {
+  local path="$1"
+  local url="${alist_host}/api/fs/list"
+  local json
+  json=$(jq -n --argjson refresh true --arg path "$path" '{refresh: $refresh, path: $path}')
 
-
-function qb_del(){
-	if [ ${leeching_mode} == "true" ]
-	then
-		if [ ${qb_v} == "1" ]
-		then
-			curl "${qb_web_url}/api/v2/torrents/delete?hashes=${file_hash}&deleteFiles=true" --cookie ${cookie}
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子名称:${torrent_name}" >> ${log_dir}/qb.log
-		elif [ ${qb_v} == "2" ]
-		then
-			curl -X POST -d "hashes=${file_hash}" "${qb_web_url}/command/deletePerm" --cookie ${cookie}
-		else
-			echo "[$(date '+%Y-%m-%d %H:%M:%S')] 删除成功！种子文件:${torrent_name}" >> ${log_dir}/qb.log
-			echo "qb_v=${qb_v}" >> ${log_dir}/qb.log
-		fi
-	else
-		echo "[$(date '+%Y-%m-%d %H:%M:%S')] 不自动删除已上传种子" >> ${log_dir}/qb.log
-	fi
+  local res
+  res=$(curl -s -X POST "$url" \
+    -H "Authorization: ${alist_token}" \
+    -d "$json")
+  local code
+  code=$(echo "$res" | jq -r '.code')
+  if [ "$code" -eq 200 ]
+  then
+    	echo "[$(date '+%Y-%m-%d %H:%M:%S')] Alist: $path 刷新成功！" >> ${log_dir}/alist.log
+  else
+	echo "[$(date '+%Y-%m-%d %H:%M:%S')] Alist: $path 刷新失败！" >> ${log_dir}/alist.log
+  fi
 }
+
 
 function rclone_copy(){
-	if [ ${type} == "file" ]
+	local_dir="${content_dir}"
+	if [ "${type}" == "file" ]
 	then
-		rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file  ${log_dir}/qbauto_copy.log "${content_dir}" ${rclone_dest}:"${save_dir:5}")
-	elif [ ${type} == "dir" ]
+		if [ "${torrent_type}" == "ani-rss" ]
+		then
+			target_dir=${save_dir}
+		else
+			target_dir=${save_dir#"$HOME"}
+		fi
+	elif [ "${type}" == "dir" ]
 	then
-		rclone_copy_cmd=$(rclone -v copy --transfers ${rclone_parallel} --log-file ${log_dir}/qbauto_copy.log "${content_dir}"/ ${rclone_dest}:"${content_dir:5}"/)
+		local_dir="${local_dir}/"
+		target_dir="${content_dir#"$HOME"}/"
+	fi
+	rclone -v copy --transfers ${rclone_parallel} --log-file  "${log_dir}"/qbauto_copy.log \
+	"${local_dir}" "${rclone_dest}:${target_dir}" \
+	--local-encoding None --onedrive-encoding None
+	if [ "${torrent_type}" == "ani-rss" ]
+	then
+		alist_refresh "/Media/Bangumi"
+		alist_refresh "${target_dir}"
 	fi
 }
 
-function qb_add_auto_del_tags(){
+function qb_uploaded(){
 	if [ ${qb_v} == "1" ]
 	then
-		curl -X POST -d "hashes=${file_hash}&tags=${auto_del_flag}" "${qb_web_url}/api/v2/torrents/addTags" --cookie "${cookie}"
+		curl -X POST -d "hashes=${torrent_hash}&tags=${uploaded_flag}" "${qb_web_url}/api/v2/torrents/addTags" --cookie "${cookie}"
 	elif [ ${qb_v} == "2" ]
 	then
-		curl -X POST -d "hashes=${file_hash}&category=${auto_del_flag}" "${qb_web_url}/command/setCategory" --cookie ${cookie}
+		curl -X POST -d "hashes=${torrent_hash}&category=${uploaded_flag}" "${qb_web_url}/command/setCategory" --cookie "${cookie}"
 	else
 		echo "qb_v=${qb_v}" >> ${log_dir}/qb.log
 	fi
@@ -102,23 +125,18 @@ function qb_add_auto_del_tags(){
 
 if [ -f "${content_dir}" ]
 then
-   echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：文件" >> ${log_dir}/qb.log
    type="file"
-   rclone_copy
-   qb_login
-   qb_add_auto_del_tags
-   qb_del
 elif [ -d "${content_dir}" ]
 then 
-   echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：目录" >> ${log_dir}/qb.log
    type="dir"
-   rclone_copy
-   qb_login
-   qb_add_auto_del_tags
-   qb_del
 else
    echo "[$(date '+%Y-%m-%d %H:%M:%S')] 未知类型，取消上传" >> ${log_dir}/qb.log
 fi
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] 类型：${type}" >> ${log_dir}/qb.log
+rclone_copy
+qb_login
+qb_uploaded
 
 echo "种子名称：${torrent_name}" >> ${log_dir}/qb.log
 echo "内容路径：${content_dir}" >> ${log_dir}/qb.log
@@ -126,7 +144,6 @@ echo "根目录：${root_dir}" >> ${log_dir}/qb.log
 echo "保存路径：${save_dir}" >> ${log_dir}/qb.log
 echo "文件数：${files_num}" >> ${log_dir}/qb.log
 echo "文件大小：${torrent_size}Bytes" >> ${log_dir}/qb.log
-echo "HASH:${file_hash}" >> ${log_dir}/qb.log
+echo "HASH:${torrent_hash}" >> ${log_dir}/qb.log
 echo "Cookie:${cookie}" >> ${log_dir}/qb.log
 echo -e "-------------------------------------------------------------\n" >> ${log_dir}/qb.log
-
